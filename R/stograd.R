e5058187ca0dd690d312bf093678c84af14f90e6
David J. H. Shih <djh.shih@gmail.com>
2019-07-05 18:13:15 -0500

    Initial commit.

---
+++ R/stograd.R
@@ -0,0 +1,116 @@
+#' Stochastic gradient descent
+#'
+#' Optimize an objective function of parameters and data with respect to
+#' the parameters using stochastic (mini-batch) gradient descent.
+#'
+#' @param param  initial guess of values for the function parameters
+#' @param data   a list of data points for input into fn
+#' @param fn     objective function (unnessary if \code{gr} is specified)
+#' @param gr     gradient of the objective function
+#' @param rate   learning rate
+#' @param control  a list of control parameters:
+#'                 \itemize{
+#'                   \item \code{nbatches}, number of batches [10];
+#'                   \item \code{bsize}, batch size;
+#'								   \item \code{nepochs}, number of passes through the data;
+#'                   \item \code{rate}, learning rate [0.01];
+#'                   \item \code{eps}, threshold for gradient norm for early exit
+#'                 }
+#' @return estimate of parameter values that minimize the objective function
+#' @export
+#'
+stograd <- function(param, data, fn, gr=NULL, control = list()) {
+	N <- length(data);
+	d <- length(param);
+
+	if (is.null(control$bsize)) {
+		if (is.null(control$nbatches)) {
+			nbatches <- 10;	
+		}
+		bsize <- floor(N / nbatches);
+	} else {
+		nbatches <- floor(N / bsize);
+	}
+
+	if (is.null(control$nepochs)) {
+		nepochs <- 100;
+	}
+
+	if (is.null(control$rate)) {
+		rate <- 1e-2;
+	}
+
+	if (is.null(control$eps)) {
+		eps <- 1e-6;
+	}
+
+	if (is.null(gr)) {
+		gr <- finite_difference_gr(fn, data);
+	}
+
+	for (i in 1:nepochs) {
+		mdelta <- rep(0, d);
+		for (b in 1:nbatches) {
+			# extract batch
+			blim <- batch_limits(b, bsize, nbatches);
+			batch <- data[blim[1]:blim[2]];
+			# actual batch size
+			B <- blim[2] - blim[1] + 1;
+
+			# update the parameters
+			delta <- rate / B * gr(param, batch);
+			param <- param - delta;
+
+			mdelta <- mdelta + delta;
+		}
+
+		# calculate mean delta
+		mdelta <- mdelta / nbatches;
+
+		# check for early convergence
+		if (sqrt(sum(mdelta*mdelta)) < eps) {
+			message("INFO: Early convergence at epoch ", i)
+			break;
+		}
+	}
+
+	param
+}
+
+# Return batch start and end indices given.
+# b         batch index
+# bsize     batch size
+# nbatches  number of batches
+batch_limits <- function(b, bsize, nbatches) {
+	# extract a batch of data
+	s <- (b - 1) * bsize + 1;
+	e <- b * bsize;
+	# group ultimate batch with the penultimate batch
+	if (b == nbatches) {
+		e <- N;
+	}
+
+	c(s, e)
+}
+
+# Central finite difference approximation the gradient.
+# Requires 2 * d evaluations of the function
+# where d is the number of parameter dimensions.
+# delta << 1e-12 appears to reduce accuracy.
+finite_difference_gr <- function(fn, data, delta=1e-12) {
+	function(param, data) {
+		d <- length(param);
+		# perturb each of d dimensions of the parameter
+		unlist(lapply(1:d,
+			function(k) {
+				# perturb only the kth dimension
+				v <- rep(0, d);
+				v[k] <- delta;
+
+				# evaluate central difference
+				(fn(param + v, data) - fn(param - v, data)) / (2*delta)
+			}
+		))
+	}
+}
+
---
+++ demo/lm.R
@@ -0,0 +1,98 @@
+# Demo application applying stochastic gradient descent
+# to solve a linear model.
+
+source("../R/stograd.R");
+
+# Gradient for linear models
+# @param  param  parameter values
+# @param  data   a list of y and X
+gradient_lm <- function(param, data) {
+	d <- unsplit_data(data);
+	y <- d[[1]];
+	X <- d[[2]];
+
+	t( -2 * t(y - X %*% param) %*% X )
+}
+
+# Squared error for linear models
+objective_lm <- function(param, data) {
+	d <- unsplit_data(data);
+	y <- d[[1]];
+	X <- d[[2]];
+
+	e <- y - X %*% param;
+	sum(e * e)
+}
+
+# Estimate linear model parameters based on normal equation:
+# \hat{\theta} = (X^\top X)^{-1} X^\top y
+estimate_by_normal_eq <- function(y, X) {
+	solve(t(X) %*% X, t(X) %*% y)
+}
+
+# Convert matrix to a list of rows
+mat_to_rlist <- function(mat) {
+	lapply(1:nrow(mat), function(i) mat[i,])
+}
+
+# Root mean squared error
+rmse <- function(x, y) {
+	sqrt(mean((x - y)^2))
+}
+
+# Split regression data into a list of observations
+# y  response variable (univariate)
+# X  predictor variables
+split_data <- function(y, X) {
+	mapply(
+		function(y, X) list(y=y, X=X),
+		y,
+		mat_to_rlist(X),
+		SIMPLIFY=FALSE
+	)
+}
+
+# Reverse the split operation on regression data
+unsplit_data <- function(data) {
+	y <- matrix(unlist(lapply(data, function(d) d$y)), ncol=1);
+	X <- matrix(unlist(lapply(data, function(d) d$X)), byrow=TRUE, nrow=length(y));
+	list(y, X)
+}
+
+
+N <- 1010;
+d <- 5;
+
+sigma <- 1;
+
+X <- matrix(rnorm(N*d), nrow=N, ncol=d);
+beta <- matrix(rnorm(d), ncol=1);
+error <- matrix(rnorm(N, sd=sigma), ncol=1);
+y <- X %*% beta + error;
+
+data <- split_data(y, X);
+
+beta0 <- matrix(rep(0, d), ncol=1);
+beta.hat.sgd <- stograd(beta0, data, fn=NULL, gr=gradient_lm);
+beta.hat.sgd2 <- stograd(beta0, data, fn=objective_lm);
+
+objective_lm(beta.hat.sgd, data)
+
+beta.hat <- estimate_by_normal_eq(y, X);
+beta.hat2 <- estimate_by_normal_eq(y[1:100, , drop=FALSE], X[1:100, ]);
+
+rmse(beta.hat.sgd, beta)
+rmse(beta.hat.sgd2, beta)
+
+rmse(beta.hat, beta)
+rmse(beta.hat2, beta)
+
+gradient_lm(beta, data)
+gradient_lm(beta.hat, data)
+gradient_lm(beta.hat.sgd, data)
+
+fdiff_lm <- finite_difference_gr(objective_lm, data);
+fdiff_lm(beta, data)
+fdiff_lm(beta.hat, data)
+fdiff_lm(beta.hat.sgd, data)
+
